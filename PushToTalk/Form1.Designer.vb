dcd739a1f96262f249eada940e6b4c3f5da108cc
James Blonden <james@blonden.co.uk>
2026-01-20 13:23:08 +0000

    Add project files.

---
+++ PushToTalk/Form1.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        NotifyIcon1 = New NotifyIcon(components)
+        PictureBox1 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' NotifyIcon1
+        ' 
+        NotifyIcon1.Text = "NotifyIcon1"
+        NotifyIcon1.Visible = True
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = My.Resources.Resources.Mic_Red
+        PictureBox1.Location = New Point(12, 12)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(150, 131)
+        PictureBox1.SizeMode = PictureBoxSizeMode.CenterImage
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(177, 156)
+        Controls.Add(PictureBox1)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Form1"
+        Text = "PTT"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents PictureBox1 As PictureBox
+
+End Class
---
+++ PushToTalk/Form1.vb
@@ -0,0 +1,151 @@
+﻿
+Imports System.Diagnostics
+Imports System.Runtime.InteropServices
+Imports NAudio.CoreAudioApi
+Imports Windows.Win32.System
+
+Public Class Form1
+    ' ===== CoreAudio (system mic) =====
+    Private mmDevice As MMDevice
+    Private isTalking As Boolean = False
+
+    ' ===== Low-level keyboard hook =====
+    Private hookId As IntPtr = IntPtr.Zero
+    Private hookProc As LowLevelKeyboardProcDelegate
+
+    Private Const WH_KEYBOARD_LL As Integer = 13
+    Private Const WM_KEYDOWN As Integer = &H100
+    Private Const WM_KEYUP As Integer = &H101
+
+    ' Choose your push-to-talk key (you can change this)
+    Private ReadOnly PttKeys As Keys() = {Keys.LControlKey, Keys.RControlKey}
+    'Private ReadOnly PttKeys As Keys() = {Keys.LeftControl, Keys.RightControl}
+
+    ' KBDLLHOOKSTRUCT for WH_KEYBOARD_LL
+    <StructLayout(LayoutKind.Sequential)>
+    Private Structure KBDLLHOOKSTRUCT
+        Public vkCode As UInteger
+        Public scanCode As UInteger
+        Public flags As UInteger
+        Public time As UInteger
+        Public dwExtraInfo As IntPtr
+    End Structure
+
+    Private Delegate Function LowLevelKeyboardProcDelegate(nCode As Integer, wParam As IntPtr, lParam As IntPtr) As IntPtr
+
+    <DllImport("user32.dll", SetLastError:=True)>
+    Private Shared Function SetWindowsHookEx(idHook As Integer, lpfn As LowLevelKeyboardProcDelegate, hMod As IntPtr, dwThreadId As UInteger) As IntPtr
+    End Function
+
+    <DllImport("user32.dll", SetLastError:=True)>
+    Private Shared Function UnhookWindowsHookEx(hhk As IntPtr) As Boolean
+    End Function
+
+    <DllImport("user32.dll", SetLastError:=True)>
+    Private Shared Function CallNextHookEx(hhk As IntPtr, nCode As Integer, wParam As IntPtr, lParam As IntPtr) As IntPtr
+    End Function
+
+    <DllImport("kernel32.dll", CharSet:=CharSet.Auto, SetLastError:=True)>
+    Private Shared Function GetModuleHandle(lpModuleName As String) As IntPtr
+    End Function
+
+    Private Function LowLevelKeyboardProc(nCode As Integer, wParam As IntPtr, lParam As IntPtr) As IntPtr
+        If nCode >= 0 Then
+            Dim data = Marshal.PtrToStructure(Of KBDLLHOOKSTRUCT)(lParam)
+            Dim key = CType(data.vkCode, Keys)
+
+            If PttKeys.Contains(key) Then
+                If wParam = CType(WM_KEYDOWN, IntPtr) Then
+                    ' Avoid repeat from key auto-repeat
+                    If Not isTalking Then
+                        SetMicMute(False)
+                        isTalking = True
+                        Try
+                            'Label1.ForeColor = System.Drawing.Color.Green
+                            'Me.BeginInvoke(Sub() Label1.Text = "Talking… (hold Ctrl)")
+                            PictureBox1.Image = My.Resources.Mic_Green
+
+                            'NotifyIcon1.Icon = My.Resources.Talk
+                            NotifyIcon1.Text = "Microphone: ACTIVE (PTT held)"
+
+                        Catch
+                        End Try
+                    End If
+
+                ElseIf wParam = CType(WM_KEYUP, IntPtr) Then
+                    ' If you’re using both Ctrl keys, you might want to track counts.
+                    If isTalking Then
+                        SetMicMute(True)
+                        isTalking = False
+                        Try
+                            'Label1.ForeColor = System.Drawing.Color.Red
+                            'Me.BeginInvoke(Sub() Label1.Text = "Muted (hold Ctrl to talk)")
+                            PictureBox1.Image = My.Resources.Mic_Red
+
+                            'NotifyIcon1.Icon = My.Resources.Mute
+                            NotifyIcon1.Text = "Microphone: Muted (hold Ctrl to talk)"
+
+
+                        Catch
+                        End Try
+                    End If
+                End If
+            End If
+        End If
+        Return CallNextHookEx(hookId, nCode, wParam, lParam)
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'Setup Notify
+        NotifyIcon1.Visible = True
+        'NotifyIcon1.Icon = My.Resources.Mute  ' starting icon
+        NotifyIcon1.Text = "Push-to-Talk Mic Controller"
+
+
+
+        ' Pick the default capture device. You can choose Role.Communications for softphones,
+        ' or Role.Multimedia / Role.Console depending on your scenario.
+        Dim enumerator = New MMDeviceEnumerator()
+
+        ' Try Communications endpoint first (commonly used by Teams/Zoom)
+        Try
+            mmDevice = enumerator.GetDefaultAudioEndpoint(Dataflow.Capture, Role.Communications)
+        Catch
+            ' Fallback to console role if communications default isn't set
+            mmDevice = enumerator.GetDefaultAudioEndpoint(Dataflow.Capture, Role.Console)
+        End Try
+
+        ' Start muted so it’s PTT by default
+        SetMicMute(True)
+
+        ' Install global keyboard hook
+        hookProc = AddressOf LowLevelKeyboardProc ' Keep delegate alive
+        Dim hModule = GetModuleHandle(Process.GetCurrentProcess().MainModule.ModuleName)
+        hookId = SetWindowsHookEx(WH_KEYBOARD_LL, hookProc, hModule, 0)
+        'Label1.ForeColor = System.Drawing.Color.Red
+        'Label1.Text = "Muted (hold Ctrl to talk)"
+        PictureBox1.Image = My.Resources.Mic_Red
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        If hookId <> IntPtr.Zero Then UnhookWindowsHookEx(hookId)
+        ' Optional: restore to unmuted on exit (or remember prior state yourself)
+        SetMicMute(False)
+    End Sub
+
+    Private Sub SetMicMute(mute As Boolean)
+        Try
+            If mmDevice IsNot Nothing Then
+                mmDevice.AudioEndpointVolume.Mute = mute
+            End If
+        Catch ex As Exception
+            ' Handle errors (device removed, access policies, etc.)
+            ' You might want to log ex.Message in a production app.
+        End Try
+    End Sub
+
+    Private Sub Form1_Load_1(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
